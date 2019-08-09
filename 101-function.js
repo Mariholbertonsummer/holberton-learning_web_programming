aa24db65b986ae65d27b005bbfbb940725a67e6a
Mari <Mariwonkert@gmail.com>
2019-08-09 08:45:13 -0400

    a
    a

---
+++ 101-function.js
@@ -0,0 +1,11 @@
+function myFunction() {
+  console.log("hello world");
+
+}
+myfunction();
+
+function mydouble(heyyaaal) {
+  console.log(heyyaaal*2);
+
+}
+mydouble(33);
---
+++ 5-javascript/102-array.js
@@ -0,0 +1,5 @@
+var numbers = [];
+for(var i =1; i <100; i++){
+numbers.push(i);
+}
+numbers;
